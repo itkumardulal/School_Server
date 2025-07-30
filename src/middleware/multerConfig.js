65d1d8632c36dd105a406880016b4a2f4e14b03b
ThapaBiplav <thapabirat95@gmail.com>
2025-07-30 08:49:44 +0545

    fix img uploaded issue

--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -1,10 +1,18 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
+// Define the uploads folder path
+const uploadPath = path.join(__dirname, "..", "..", "uploads");
+
+// Ensure uploads folder exists, create if missing
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "..", "..", "uploads")); // Create /uploads directory if not exists
+    cb(null, uploadPath); // Use the ensured folder path
   },
   filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}-${file.originalname}`;
@@ -12,34 +20,25 @@ const storage = multer.diskStorage({
   },
 });
 
-
 const fileFilter = (req, file, cb) => {
-  const allowedImageTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/jpg",
-  ];
-
+  const allowedImageTypes = ["image/jpeg", "image/png", "image/jpg"];
 
   if (allowedImageTypes.includes(file.mimetype)) {
     req.uploadType = "image";
     cb(null, true);
   } else {
-    cb(new Error("Unsupported file type. Only images  are allowed."), false);
+    cb(new Error("Unsupported file type. Only images are allowed."), false);
   }
 };
-
 
 const singleUpload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 10 * 1024 * 1024, // 1MB
+    fileSize: 1 * 1024 * 1024, 
   },
 }).single("file");
 
-
 module.exports = {
   singleUpload,
- 
 };
