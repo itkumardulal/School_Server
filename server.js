31036821edbff298ad95dc786c7f54e323cccf7f
ThapaBiplav <thapabirat95@gmail.com>
2025-08-13 21:01:08 +0545

    updated code

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const { envPort } = require("./src/config/config")
 
 
 function startServer(){
-  const PORT = envPort.port
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
+ const PORT = process.env.PORT || 4000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 }
 
 startServer()
